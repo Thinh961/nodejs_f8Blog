254f1361a3dc3356e27ba6497d11be514ccf1ef2
Thinh99 <ncthinh.199@gmail.com>
2022-08-02 18:05:13 +0700

    thinh

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -2,16 +2,10 @@ const Course = require('../models/CourseModel')
 class SiteController {
 
     // [GET] /
-    index(req, res) {
-        Course.find({}, function(err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'ERROR!!!'});
-            }
-        });
-        // res.render('home');
-        // thinh
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => res.render('home', { courses }))
+            .catch(next);
     }
 
     // [GET] /search
